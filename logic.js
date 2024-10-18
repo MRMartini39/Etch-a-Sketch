ea2fff2c2b12a2c62f075dea2758233999659d67
MRMartini <michael.martini6455@gmail.com>
2024-10-18 18:33:27 -0500

    Added mouseover effect to grid Added eventlisteners to trigger upon mouseover

--- logic.js
+++ logic.js
@@ -6,6 +6,10 @@ for (let i = 0; i < 16; i++) {
     for (let j = 0; j < 16; j++) {
         const pixel = document.createElement("div");
         pixel.setAttribute("class", "pixel");
+        pixel.addEventListener("mouseover", () => {
+            pixel.setAttribute("id", "black");
+        })
+
         row.appendChild(pixel);
     }
     container.appendChild(row);
