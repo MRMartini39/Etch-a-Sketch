7caa78f8a87a990ac5ecb737bde54c0f3f0dfb28
MRMartini <michael.martini6455@gmail.com>
2024-10-18 19:26:08 -0500

    Added button that allows user to set grid size Added button to html, style to css, and added an additional layer of abstraction in js logic

--- logic.js
+++ logic.js
@@ -1,16 +1,42 @@
 const container = document.querySelector("#container");
+const button = document.querySelector("#setGrid");
 
-for (let i = 0; i < 16; i++) {
-    const row = document.createElement("div");
-    row.setAttribute("class", "row");
-    for (let j = 0; j < 16; j++) {
-        const pixel = document.createElement("div");
-        pixel.setAttribute("class", "pixel");
-        pixel.addEventListener("mouseover", () => {
-            pixel.setAttribute("id", "black");
-        })
+button.addEventListener("click", () => {
+    let gridSize = prompt("How many squares per side?");
+    createGrid(gridSize);
+})
 
-        row.appendChild(pixel);
+function createGrid(gridSize) {
+    removeGrid();
+
+    if (gridSize > 100) {
+        gridSize = 100;
     }
-    container.appendChild(row);
+
+    for (let i = 0; i < gridSize; i++) {
+        const row = document.createElement("div");
+        row.setAttribute("class", "row");
+        for (let j = 0; j < gridSize; j++) {
+            const pixel = document.createElement("div");
+            pixel.setAttribute("class", "pixel");
+            pixel.addEventListener("mouseover", () => {
+                pixel.setAttribute("id", "black");
+            })
+    
+            row.appendChild(pixel);
+        }
+        container.appendChild(row);
+    }
 }
+
+function removeGrid() {
+    const container = document.querySelector("#container");
+    const children = Array.from(document.querySelectorAll(".row"));
+
+    children.forEach((child) => {
+        container.removeChild(child);
+
+    });
+}
+
+createGrid(16);
